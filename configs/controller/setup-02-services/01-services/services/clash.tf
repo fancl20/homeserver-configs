bbc25924a001a4be0a6a2c612c94c8fa171e335f
root <fancl20@gmail.com>
2022-08-09 21:37:44 +1000

    Clash use tun+ebpf

--- configs/controller/setup-02-services/01-services/services/clash.tf
+++ configs/controller/setup-02-services/01-services/services/clash.tf
@@ -4,16 +4,25 @@ resource "kubernetes_config_map" "clash" {
   }
   data = {
     "config.yaml" = <<-EOT
-    tproxy-port: 7893
-
     external-controller: 0.0.0.0:9090
     external-ui: clash-dashboard-gh-pages
 
     interface-name: net1
 
+    ipv6: false
+
+    routing-mark: 1
+    tun:
+      enable: true
+      stack: system
+
+    ebpf:
+      redirect-to-tun:
+        - net1
+
     dns:
       enable: true
-      listen: 127.0.0.1:1153
+      listen: 0.0.0.0:53
       enhanced-mode: fake-ip
       fake-ip-range: 198.18.0.0/16
       nameserver:
@@ -53,19 +62,6 @@ resource "kubernetes_config_map" "clash" {
           # Default
           return "DIRECT"
     EOT
-
-    "Corefile" = <<-EOT
-    . {
-      forward . 192.168.1.1
-      log
-      errors
-    }
-    #mihoyo.com {
-    #  forward . 127.0.0.1:1153
-    #  log
-    #  errors
-    #}
-    EOT
   }
 }
 
@@ -76,7 +72,6 @@ locals {
       name = kubernetes_config_map.clash.metadata[0].name
       items = [
         { key = "config.yaml", path = "config.yaml" },
-        { key = "Corefile", path = "Corefile" },
       ]
     }
   }
@@ -88,55 +83,25 @@ module "clash_dns" {
   deployment = {
     containers = [
       {
-        name    = "clash-dns"
-        image   = "coredns/coredns"
-        command = ["/coredns", "-conf", "/config/Corefile"]
-        env = [
-          { name = "TZ", value = "Australia/Sydney" },
-        ]
-        volumeMounts = [
-          { name = "config", mountPath = "/config" },
-        ]
-      },
-      {
         name  = "clash"
         image = "ghcr.io/fancl20/clash"
         command = ["/bin/sh", "-e", "-c", <<-EOT
           cat /etc/config/config.yaml /vault/secrets/proxies > /root/.config/clash/config.yaml
 
-          NETFILTER_MARK=1
-          IPROUTE2_TABLE_ID=100
+          echo -e "nameserver 8.8.8.8\n$(cat /etc/resolv.conf)" > /etc/resolv.conf
+
+          mkdir -p /dev/net
+          mknod /dev/net/tun c 10 200
+          chmod 600 /dev/net/tun
 
           ip route add 10.96.0.0/12 via 10.244.0.1 # serviceCIDR
           ip route replace default via 192.168.1.1 dev net1
-          ip route replace local default dev lo table "$IPROUTE2_TABLE_ID"
-          ip rule add fwmark "$NETFILTER_MARK" lookup "$IPROUTE2_TABLE_ID"
-
-          nft -f - << EOF
-          table clash
-          flush table clash
-          table clash {
-            chain input {
-              type filter hook prerouting priority mangle; policy accept;
-              ip protocol != { tcp, udp } accept
-              ip daddr { 127.0.0.0/8, 224.0.0.0/4, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, 169.254.0.0/16, 240.0.0.0/4 } accept
-
-              ip daddr { 192.18.0.0/16, 203.107.0.0/16, 101.226.0.0/16, 138.113.112.0/24, 163.171.210.0/24 } goto proxy
-              ip daddr { 43.142.125.244 } goto proxy # Project Zomboid
-              udp dport { 22101, 22102 } goto proxy
-            }
-            chain proxy {
-              ip protocol tcp mark set $NETFILTER_MARK tproxy to 127.0.0.1:7893
-              ip protocol udp mark set $NETFILTER_MARK tproxy to 127.0.0.1:7893
-            }
-          }
-          EOF
 
           exec /opt/bin/clash
           EOT
         ]
         securityContext = {
-          capabilities = { add = ["NET_ADMIN"] }
+          capabilities = { add = ["NET_ADMIN", "SYS_MODULE", "SYS_ADMIN"] }
         }
         env = [
           { name = "TZ", value = "Australia/Sydney" },
