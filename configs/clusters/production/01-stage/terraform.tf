db9bc09e6f447f83b046a8b82706f3d21cd03caf
root <fancl20@gmail.com>
2023-04-08 00:15:07 +1000

    flux: disable gcs backend config

--- configs/clusters/production/01-stage/terraform.tf
+++ configs/clusters/production/01-stage/terraform.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket      = "0c9cfd1d-dc77-43e6-9131-d5535a91d844"
-    prefix      = "terraform/state/clusters/production/01-stage"
-    credentials = var.google_credentials
+    bucket = "0c9cfd1d-dc77-43e6-9131-d5535a91d844"
+    prefix = "terraform/state/clusters/production/01-stage"
   }
 }
 
