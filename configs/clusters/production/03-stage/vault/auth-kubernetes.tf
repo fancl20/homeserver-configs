04e7e4de424b2bbdaa30a36a9e0d299e8fcb1586
root <fancl20@gmail.com>
2023-04-11 01:14:24 +1000

    flux: remove vault k8s auth config

--- configs/clusters/production/03-stage/vault/auth-kubernetes.tf
+++ configs/clusters/production/03-stage/vault/auth-kubernetes.tf
@@ -2,7 +2,8 @@ resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
 
-resource "vault_kubernetes_auth_backend_config" "kubernetes" {
-  backend         = vault_auth_backend.kubernetes.path
-  kubernetes_host = "https://kubernetes.default.svc.cluster.local"
-}
+# TODO: FIXME
+# resource "vault_kubernetes_auth_backend_config" "kubernetes" {
+#   backend         = vault_auth_backend.kubernetes.path
+#   kubernetes_host = "https://kubernetes.default.svc.cluster.local"
+# }
