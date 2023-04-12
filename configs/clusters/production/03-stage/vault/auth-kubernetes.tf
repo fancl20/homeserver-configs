b5bd398bd845f52d972b329316abdb585ff26068
root <fancl20@gmail.com>
2023-04-12 20:56:21 +1000

    flux: fix vault_kubernetes_auth_backend_config

--- configs/clusters/production/03-stage/vault/auth-kubernetes.tf
+++ configs/clusters/production/03-stage/vault/auth-kubernetes.tf
@@ -2,8 +2,7 @@ resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
 
-# TODO: FIXME
-# resource "vault_kubernetes_auth_backend_config" "kubernetes" {
-#   backend         = vault_auth_backend.kubernetes.path
-#   kubernetes_host = "https://kubernetes.default.svc.cluster.local"
-# }
+resource "vault_kubernetes_auth_backend_config" "kubernetes" {
+  backend         = vault_auth_backend.kubernetes.path
+  kubernetes_host = "https://kubernetes.default.svc"
+}
