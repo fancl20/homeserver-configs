333c1c701b2620ce7f7bb9ef196ece1f8284b2e5
root <fancl20@gmail.com>
2023-08-06 19:56:01 +1000

    add source-controller role

--- configs/clusters/production/03-stage/vault/secrets-operator.tf
+++ configs/clusters/production/03-stage/vault/secrets-operator.tf
@@ -15,3 +15,21 @@ resource "vault_kubernetes_auth_backend_role" "notifier" {
   bound_service_account_namespaces = ["flux-system"]
   token_policies                   = ["default", vault_policy.notifier.name]
 }
+
+# Source controller require github personal access token for updating source.
+resource "vault_policy" "source_controller" {
+  name   = "source-controller"
+  policy = <<-EOT
+    path "homeserver/data/github_pat" {
+      capabilities = ["read"]
+    }
+  EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "source_controller" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "source-controller"
+  bound_service_account_names      = ["source-controller"]
+  bound_service_account_namespaces = ["flux-system"]
+  token_policies                   = ["default", vault_policy.source_controller.name]
+}
