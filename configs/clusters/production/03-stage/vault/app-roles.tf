e82a24c392f49f948c98a3a3ee1f999e2adb1762
root <fancl20@gmail.com>
2024-02-11 13:46:28 +1100

    update base image

--- configs/clusters/production/03-stage/vault/app-roles.tf
+++ configs/clusters/production/03-stage/vault/app-roles.tf
@@ -87,24 +87,6 @@ resource "vault_kubernetes_auth_backend_role" "data_ssh" {
   token_policies                   = ["default", vault_policy.data_ssh.name]
 }
 
-# Clash & dae requires proxy information.
-resource "vault_policy" "proxy" {
-  name   = "kubernetes_proxy"
-  policy = <<-EOT
-    path "homeserver/data/clash" {
-      capabilities = ["read"]
-    }
-  EOT
-}
-
-resource "vault_kubernetes_auth_backend_role" "proxy" {
-  backend                          = vault_auth_backend.kubernetes.path
-  role_name                        = "proxy"
-  bound_service_account_names      = ["clash", "dae"]
-  bound_service_account_namespaces = ["default"]
-  token_policies                   = ["default", vault_policy.proxy.name]
-}
-
 # Workspace requires private key for using Git inside.
 resource "vault_policy" "workspace_ssh" {
   name   = "kubernetes_workspace_ssh"
