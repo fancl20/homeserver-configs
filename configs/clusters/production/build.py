af6d663d68c6e4a86804c893d556ec464f9b3f6f
root <fancl20@gmail.com>
2024-12-02 08:52:34 +1100

    remove argo

--- configs/clusters/production/build.py
+++ configs/clusters/production/build.py
@@ -7,20 +7,15 @@ import subprocess
 import sys
 import tarfile
 import urllib.request
-from typing import Any, Dict
-
-
-def github_latest(repo: str) -> Dict[str, Any]:
-  with urllib.request.urlopen(f'https://api.github.com/repos/{repo}/releases/latest') as r:
-    return json.load(r)
 
 
 def install_flux(dst: pathlib.Path):
   if dst.joinpath('flux').exists():
     return
-  for a in github_latest('fluxcd/flux2')['assets']:
-    if 'linux_amd64' in a['name']:
-      u = a['browser_download_url']
+  with urllib.request.urlopen('https://api.github.com/repos/fluxcd/flux2/releases/latest') as r:
+    for a in json.load(r)['assets']:
+      if 'linux_amd64' in a['name']:
+        u = a['browser_download_url']
   with urllib.request.urlopen(u) as r:
     with tarfile.open(fileobj=r, mode='r|*') as z:
       z.extractall(dst)
@@ -54,13 +49,22 @@ def main():
       'https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset.yml',
   )
 
-  # Argo Workflows
+  # Tekton
   get_url(
-      pathlib.Path('00-stage', 'argo', 'workflows.yaml'),
-      {
-          a['name']: a['browser_download_url']
-          for a in github_latest('argoproj/argo-workflows')['assets']
-      }['install.yaml']
+      pathlib.Path('00-stage', 'tekton-pipelines', 'pipelines.yaml'),
+      'https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml',
+  )
+  get_url(
+      pathlib.Path('00-stage', 'tekton-pipelines', 'triggers.yaml'),
+      'https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml',
+  )
+  get_url(
+      pathlib.Path('00-stage', 'tekton-pipelines', 'interceptors.yaml'),
+      'https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml',
+  )
+  get_url(
+      pathlib.Path('00-stage', 'tekton-pipelines', 'dashboard.yaml'),
+      'https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml',
   )
 
   # 99-default
