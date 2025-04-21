3c3f37de987dbaee44eb8a33c2118b78b086a74e
fancl20 <fancl20@gmail.com>
2025-04-21 03:08:11 +0000

    add multus & tekton

--- configs/clusters-ng/production/build.py
+++ configs/clusters-ng/production/build.py
@@ -16,6 +16,13 @@ def get_url(dst: pathlib.Path, url: str):
         f.write(b)
 
 
+def get_github_latest_release(repo: str):
+  req = urllib.request.Request(f"https://api.github.com/repos/{repo}/releases/latest")
+  with urllib.request.urlopen(req) as r:
+    d = json.loads(r.read().decode())
+    return d["tag_name"].lstrip('v')
+
+
 def main():
   # Flux
   with pathlib.Path('00-stage', 'flux', 'gotk-components.yaml').open('w') as f:
@@ -26,29 +33,23 @@ def main():
         '--components-extra=image-reflector-controller,image-automation-controller',
     ], stdout=f)
 
-  ## Multus-cni
-  #get_url(
-  #    pathlib.Path('00-stage', 'multus-cni', 'multus-daemonset.yaml'),
-  #    'https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset.yml',
-  #)
+  # Multus-cni
+  multus_ver = get_github_latest_release('k8snetworkplumbingwg/multus-cni')
+  get_url(
+      pathlib.Path('04-stage', 'multus-cni', 'multus-daemonset.yaml'),
+      'https://raw.githubusercontent.com/'
+      'k8snetworkplumbingwg/multus-cni/'
+      f'refs/tags/v{multus_ver}/'
+      'deployments/multus-daemonset.yml',
+  )
 
   ## Tekton
-  #get_url(
-  #    pathlib.Path('00-stage', 'tekton-pipelines', 'pipelines.yaml'),
-  #    'https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml',
-  #)
-  #get_url(
-  #    pathlib.Path('00-stage', 'tekton-pipelines', 'triggers.yaml'),
-  #    'https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml',
-  #)
-  #get_url(
-  #    pathlib.Path('00-stage', 'tekton-pipelines', 'interceptors.yaml'),
-  #    'https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml',
-  #)
-  #get_url(
-  #    pathlib.Path('00-stage', 'tekton-pipelines', 'dashboard.yaml'),
-  #    'https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml',
-  #)
+  tekton_ver = get_github_latest_release('tektoncd/operator')
+  get_url(
+      pathlib.Path('04-stage', 'tekton', 'tekton-operator.yaml'),
+      'https://storage.googleapis.com/tekton-releases/'
+      f'operator/previous/v{tekton_ver}/release.yaml',
+  )
 
   ## 99-default
   #subprocess.check_call([
