6458599ad55346c632f127414271535285596937
default <fancl20@gmail.com>
2026-06-13 09:24:07 +0000

    coder: glm 5.1 -> 5.2

--- configs/clusters/production/99-services/03-applications/coder.d/kubernetes-devcontainer/main.tf
+++ configs/clusters/production/99-services/03-applications/coder.d/kubernetes-devcontainer/main.tf
@@ -273,11 +273,11 @@ resource "kubernetes_deployment_v1" "main" {
           }
           env {
             name  = "ANTHROPIC_DEFAULT_OPUS_MODEL"
-            value = "glm-5.1"
+            value = "glm-5.2"
           }
           env {
             name  = "ANTHROPIC_DEFAULT_SONNET_MODEL"
-            value = "glm-5.1"
+            value = "glm-5.2"
           }
           env {
             name  = "ANTHROPIC_DEFAULT_HAIKU_MODEL"
